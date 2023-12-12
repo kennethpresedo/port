fb204eb09077374a2bd82af202be109c394f551b
Kenneth Presedo <kennethpresedo@live.com>
2023-12-11 20:06:20 -0500

    init

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Home.css';
+
+const Home = () => {
+  return (
+    <div className='head'>Kenneth Presedo <br/>
+     <span className='nav'>About Projects Contact </span>
+    </div>
+  
+  )
+}
+
+export default Home
