d1cc83a4a1177b7e1be1c5eb748c169368b5a1da
Kenneth Presedo <kennethpresedo@live.com>
2024-01-11 00:24:31 -0500

    single pages edit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,71 @@
+import About from '../About/About';
+import Contact from '../Contact/Contact';
+import Projects from '../Projects/Projects';
 import styles from './Header.module.scss'
-import React from 'react';
-import { Link } from 'react-scroll';
+import React, { useState } from 'react';
 
 export default function Header() {
+  const [isVisibleProjects, setIsVisibleProjects] = useState(false);
+  const [isVisibleAbout, setIsVisibleAbout] = useState(false);
+  const [isVisibleContact, setIsVisibleContact] = useState(false);
+
+  
+  const handleProjectsClick = () => {
+    setIsVisibleProjects(!isVisibleProjects);
+    setIsVisibleAbout(false);
+    setIsVisibleContact(false);
+  };
+
+  const handleAboutClick = () => {
+    setIsVisibleAbout(!isVisibleAbout);
+    setIsVisibleProjects(false);
+    setIsVisibleContact(false);
+  };
+
+  const handleContactClick = () => {
+    setIsVisibleContact(!isVisibleContact);
+    setIsVisibleProjects(false);
+    setIsVisibleAbout(false);
+  };
 
   return (
     <>
       <div className={styles.container}>
         <div className={styles.head}>Kenneth Presedo</div><br />
         <div className={styles.nav}>
+        <button className={styles.nav} onClick={handleProjectsClick}>
+        {isVisibleProjects ? 'Projects' : 'Projects'}
+      </button> 
+
+      <button className={styles.nav} onClick={handleAboutClick}>
+        {isVisibleAbout ? 'About' : 'About'}
+      </button>
+
+      <button className={styles.nav} onClick={handleContactClick}>
+        {isVisibleContact ? 'Contact' : 'Contact'}
+      </button>
 
 
-  
-          <Link to="projectsSection" smooth={true} duration={800}>
-            <span className={styles.projects}>Projects</span>&nbsp;
-          </Link>
+         </div>
+      {isVisibleProjects && <Projects />}
+      {isVisibleAbout && <About />}
+      {isVisibleContact && <Contact />}
 
-
-          <Link to="aboutSection" smooth={true} duration={800}>
-            <span className={styles.about}>About</span>&nbsp;
-          </Link>
-
-          <Link to="contactSection" smooth={true} duration={800}>
-            <span className={styles.contact}>Contact</span>
-          </Link>
-
-        </div>
       </div>
     </>
   )
 }
+
+
+{/* <Link to="projectsSection" smooth={true} duration={800}>
+<span className={styles.projects}>Projects</span>&nbsp;
+</Link>
+
+
+<Link to="aboutSection" smooth={true} duration={800}>
+<span className={styles.about}>About</span>&nbsp;
+</Link>
+
+<Link to="contactSection" smooth={true} duration={800}>
+<span className={styles.contact}>Contact</span>
+</Link> */}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,17 @@
 //import React from 'react'
 import React from 'react';
-import { Element } from 'react-scroll'
+//import { Element } from 'react-scroll'
 import Header from '../../components/Header/Header'
-import About from '../../components/About/About'
-import Projects from '../../components/Projects/Projects'
-import Contact from '../../components/Contact/Contact'
+// import About from '../../components/About/About'
+// import Projects from '../../components/Projects/Projects'
+// import Contact from '../../components/Contact/Contact'
 
 export default function Home() {
 
   return (
     <>
       <Header />
-      <Element name="projectsSection">
+      {/* <Element name="projectsSection">
         <Projects />
       </Element>
 
@@ -21,7 +21,7 @@ export default function Home() {
 
       <Element name="contactSection">
         <Contact />
-      </Element>
+      </Element> */}
     </>
   )
 }
