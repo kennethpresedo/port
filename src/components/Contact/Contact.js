6d4b15e6c187f1cc55cf2045020b2aa6ed095aec
Kenneth Presedo <kennethpresedo@live.com>
2024-01-15 16:51:30 -0500

    contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import styles from './Contact.module.scss'
+import linkedinLogo from '../linkedinLogo.png'
 
 export default function Contact() {
 
@@ -10,10 +11,17 @@ export default function Contact() {
                     Contact
                 </div>
                 <div className={styles.contact}>
-                    <a className={styles.socialsLinkedin} href="https://www.linkedin.com/in/kenneth-presedo/" target="_blank" rel="noopener noreferrer">
-                        LinkedIn
-                    </a>
-                    resume email
+                    <div className={styles.left}>
+                    KP
+                    </div>
+                    <div className={styles.break}></div>
+                    <div className={styles.right}><div>
+                        <a className={styles.socialsLinkedin} href="https://www.linkedin.com/in/kenneth-presedo/" target="_blank" rel="noopener noreferrer">
+                            <img className={styles.logo} src={linkedinLogo} alt='logo' width='55vw' />
+                        </a></div>
+                        <div className={styles.arroba}>@</div>
+                        <div className={styles.hash}>#</div>
+                    </div>
                 </div>
             </div>
 
