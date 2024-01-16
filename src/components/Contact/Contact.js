056fb733bad113fb9fc602693a74f34f98359a77
Kenneth Presedo <kennethpresedo@live.com>
2024-01-15 22:17:17 -0500

    contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,30 +1,49 @@
 import styles from './Contact.module.scss'
 import linkedinLogo from '../linkedinLogo.png'
+import location from '../location.png'
+import at from '../at.png'
+import call from '../call.png'
 
 export default function Contact() {
 
     return (
         <>
-
             <div className={styles.container}>
                 <div className={styles.head}>
                     Contact
                 </div>
                 <div className={styles.contact}>
                     <div className={styles.left}>
-                    KP
+                        KP
                     </div>
                     <div className={styles.break}></div>
-                    <div className={styles.right}><div>
-                        <a className={styles.socialsLinkedin} href="https://www.linkedin.com/in/kenneth-presedo/" target="_blank" rel="noopener noreferrer">
-                            <img className={styles.logo} src={linkedinLogo} alt='logo' width='55vw' />
-                        </a></div>
-                        <div className={styles.arroba}>@</div>
-                        <div className={styles.hash}>#</div>
+                    <div className={styles.right}>
+                            <div className={styles.top}>
+                            <a className={styles.socialsLinkedin} href="https://www.linkedin.com/in/kenneth-presedo/" target="_blank" rel="noopener noreferrer">
+                                <img className={styles.logo} src={linkedinLogo} alt='logo' width='55vw' />
+                            </a>
+                            <a className={styles.socialsEmail} href="mailto:kennethpresedo@live.com?subject=Just saw your portfolio!&body=Just wanted to let you know that, YOU ROCK!">
+                                <div className={styles.arroba}>
+                                    <img className={styles.pin} src={at} alt='logo' width='55vw' />
+                                </div>
+                            </a>
+                            <div className={styles.hash}>
+                                <img className={styles.pin} src={call} alt='logo' width='55vw' />
+                            </div>
+                            <div className={styles.pin}>
+                                <img className={styles.pin} src={location} alt='logo' width='55vw' />
+                            </div>
+                        </div>
+                        <div className={styles.bottom}>
+                            in/kenneth-presedo< br />
+                            kennethpresedo@live.com<br />
+                            786-247-3451<br />
+                            miami, fl
+                        </div>
                     </div>
+
                 </div>
             </div>
-
         </>
     )
 }
